1f99b81c56d4825d6293f78b2c1fb4ecab720933
Mehmet Ergün <mrmehmetergun@gmail.com>
2023-02-24 15:46:49 -0500

    Initial Commit

---
+++ FruitVocabulary/FruitVocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitVocabularyApp.swift
+//  FruitVocabulary
+//
+//  Created by Mehmet Ergün on 2023-02-24.
+//
+
+import SwiftUI
+
+@main
+struct FruitVocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
